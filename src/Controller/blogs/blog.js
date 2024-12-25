ddbefda3945ecd9ba84ef2b32197ef7f1e385275
vishalsinh <vishalsinhparmar821@gamil.com>
2024-12-25 21:50:08 +0530

    updateBLog

--- src/Controller/blogs/blog.js
+++ src/Controller/blogs/blog.js
@@ -1,200 +1,206 @@
 const Blog = require('../../Model/blogs/blogs.model.js');
 const Comment = require('../../Model/blogs/comment.model.js')
-const createBlog = async (req,res)=>{
-    const {title,description}= req.body;
-    
+const createBlog = async (req, res) => {
+    const { title, description } = req.body;
+
     console.log(`the title:${title} description:${description}`);
 
-    try{
-        console.log('the req.file',req.file)
+    try {
+        console.log('the req.file', req.file)
         const blogImage = req.file.path;
-     
-        console.log('the blog image is ',blogImage);
+
+        console.log('the blog image is ', blogImage);
         const blogpost = await Blog.create({
-            filepath:blogImage,
+            filepath: blogImage,
             title,
             description,
-            authorId:req.user.sub
-         
+            authorId: req.user.sub
+
         })
 
-        res.status(201).send({message:'the blog post is created successfully',blog:blogpost})
+        res.status(201).send({ message: 'the blog post is created successfully', blog: blogpost })
 
-    }catch(err){
+    } catch (err) {
         // return res.status(401).send({message:'false',res:err.message})
         console.log(err.message)
     }
 };
 
-const showBlog = async (req,res) =>{
-    const {page = 1 ,limit=3} = req.query;
+const showBlog = async (req, res) => {
+    const { page = 1, limit = 3 } = req.query;
 
-    const skip = (page-1)*limit;
-    try{
-         
-         const blogpost = await Blog.find().limit(Number(limit)).skip(skip).populate("authorId");
-         const blogCount = await Blog.countDocuments();
+    const skip = (page - 1) * limit;
+    try {
 
-         res.status(200).send({
-            message:'the blog post is created successfully',
+        const blogpost = await Blog.find().limit(Number(limit)).skip(skip).populate("authorId");
+        const blogCount = await Blog.countDocuments();
+
+        res.status(200).send({
+            message: 'the blog post is created successfully',
             blogpost,
             blogCount,
-            page:Number(page),
-            pages:Math.ceil(blogCount/limit)
+            page: Number(page),
+            pages: Math.ceil(blogCount / limit)
         })
-        
 
-    }catch(err){
-        res.status(401).send({message:'false',res:err.message})
-        
-    }
-};
 
-const showBlogbyid = async (req,res) =>{
-    
-    const {id} = req.params;
-    
-    try{
-    if(!id){
-    res.status(401).send({message:'i have not find any kind of blog'});
-      
-    }
-    const blogPost = await Blog.findById(id).populate('authorId');
-
-    res.status(200).send(blogPost);
-    }catch(err){
-    res.status(401).send({message:err.messsage});
+    } catch (err) {
+        res.status(401).send({ message: 'false', res: err.message })
 
     }
 };
 
-const showBloguser = async (req,res)=>{
-     try{
-        console.log('the user id is a ',req.user.sub)
-    const blog = await Blog.find({authorId:req.user.sub});
-    console.log("the user blog is",blog)
-    res.status(200).send({blog});
-     }catch(err){
-    res.status(401).send({message:err.messsage});
-     }
+const showBlogbyid = async (req, res) => {
 
-    
-}
-const showUserblogbyId = async (req,res)=>{
-    const {id} = req.params
-    try{
-       console.log('the user id is a ',req.user.sub)
-   const blog = await Blog.find({authorId:id}).sort({createdAt:-1});
-   console.log("the user blog is",blog)
-   res.status(200).send({blog});
-    }catch(err){
-   res.status(401).send({message:err.messsage});
+    const { id } = req.params;
+
+    try {
+        if (!id) {
+            res.status(401).send({ message: 'i have not find any kind of blog' });
+
+        }
+        const blogPost = await Blog.findById(id).populate('authorId');
+
+        res.status(200).send(blogPost);
+    } catch (err) {
+        res.status(401).send({ message: err.messsage });
+
+    }
+};
+
+const showBloguser = async (req, res) => {
+    try {
+        console.log('the user id is a ', req.user.sub)
+        const blog = await Blog.find({ authorId: req.user.sub });
+        console.log("the user blog is", blog)
+        res.status(200).send({ blog });
+    } catch (err) {
+        res.status(401).send({ message: err.messsage });
     }
 
-   
+
 }
-const userBlogUpdateshowbyid = async (req,res)=>{
-    const {id} = req.params;
-    
+const showUserblogbyId = async (req, res) => {
+    const { id } = req.params
+    try {
+        console.log('the user id is a ', req.user.sub)
+        const blog = await Blog.find({ authorId: id }).sort({ createdAt: -1 });
+        console.log("the user blog is", blog)
+        res.status(200).send({ blog });
+    } catch (err) {
+        res.status(401).send({ message: err.messsage });
+    }
+
+
+}
+const userBlogUpdateshowbyid = async (req, res) => {
+    const { id } = req.params;
+
     // console.log('the updated data sent by client',updateData)
-    try{
-   const blog = await Blog.findById(id);
-   console.log("the user blog is ",blog)
-   res.status(200).send(blog);
-    }catch(err){
-   res.status(401).send({message:err.messsage});
+    try {
+        const blog = await Blog.findById(id);
+        console.log("the user blog is ", blog)
+        res.status(200).send(blog);
+    } catch (err) {
+        res.status(401).send({ message: err.messsage });
     }
 }
 //  delete the blog 
-const userUpdateBlog = async (req,res)=>{
-    const {id} = req.params;
-    const {title,description} = req.body;
-    const blogImage = req.file.path;
-   
-
+const userUpdateBlog = async (req, res) => {
+    const { id } = req.params;
+    const { title, description } = req.body;
+    
+    
     
     // console.log('the updated data sent by client',updateData)
-    try{
-   const blog = await Blog.findOneAndUpdate({_id:id},{
-     title,
-     description,
-     filepath:blogImage
-     
-   },{new:true});
-   console.log("the user blog is",blog)
-   res.status(200).send(blog);
-    }catch(err){
-        console.log("the error message is",err.message)
-   return res.status(401).send({message:err.messsage});
-   
+    try {
+        const findBlog = await Blog.findById(id);
+        
+        console.log(findBlog)
+        const blogImage = req.file?.path;
+        const updateobj = {
+            filepath: blogImage || findBlog.filepath,
+            title,
+            description
+        }
+        const blog = await Blog.findOneAndUpdate({ _id:id }, 
+            updateobj
+            , 
+            { new: true });
+        console.log("the user blog is", blog)
+        res.status(200).send(blog);
+    } catch (err) {
+        console.log("the error message is", err.message)
+        return res.status(401).send({ message: err.messsage });
+
     }
 
-   
+
 }
-const userdeleteBlog = async (req,res)=>{
-    const {id} = req.params;
-    try{
-   const blog = await Blog.deleteOne({_id:id});
-   console.log("the user blog is",blog)
-   res.status(200).send(blog);
-    }catch(err){
-   res.status(401).send({message:err.messsage});
+const userdeleteBlog = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const blog = await Blog.deleteOne({ _id: id });
+        console.log("the user blog is", blog)
+        res.status(200).send(blog);
+    } catch (err) {
+        res.status(401).send({ message: err.messsage });
     }
 
-   
+
 }
 //  comment functionalitty in the blog
-    const commentData = async (req,res) =>{
-        const {comment} = req.body;
-        console.log("the comment for the comment sec",comment);
-        const {blogid} = req.params
-        console.log("the blog id is",blogid);
+const commentData = async (req, res) => {
+    const { comment } = req.body;
+    console.log("the comment for the comment sec", comment);
+    const { blogid } = req.params
+    console.log("the blog id is", blogid);
 
-        try{
-            const newComment = await Comment.create({
-                comment,
-                userid:req.user.sub,
-                blogid
-              
-            });
+    try {
+        const newComment = await Comment.create({
+            comment,
+            userid: req.user.sub,
+            blogid
 
-            await Blog.findByIdAndUpdate(
-                blogid ,
-                 {
-                    $push:{comments:newComment._id}
-                 }
-            );
+        });
 
-            console.log('the newComment is',newComment);
+        await Blog.findByIdAndUpdate(
+            blogid,
+            {
+                $push: { comments: newComment._id }
+            }
+        );
 
-            res.status(200).send({message:'the comment created successfully'});
-        }catch(err){
-            console.log('error occur in comment',err.message)
-        }
-    };
+        console.log('the newComment is', newComment);
 
-    const commentDatashow = async (req,res) =>{
-        const {blogid} = req.params
+        res.status(200).send({ message: 'the comment created successfully' });
+    } catch (err) {
+        console.log('error occur in comment', err.message)
+    }
+};
 
-        try{
-          
-            const commentData = await Comment.find({blogid}).populate("userid","username filepath");
-             const commentCount = await Comment.find({blogid}).countDocuments();
+const commentDatashow = async (req, res) => {
+    const { blogid } = req.params
 
-            console.log('the comment data in the commentDatasshow',commentData,commentCount);
+    try {
+
+        const commentData = await Comment.find({ blogid }).populate("userid", "username filepath");
+        const commentCount = await Comment.find({ blogid }).countDocuments();
+
+        console.log('the comment data in the commentDatasshow', commentData, commentCount);
 
 
-       
-            res.status(200).send({commentData,commentCount});
-         
-        }catch(err){
-            console.log('error occur in comment',err.message)
-        }
-    };
+
+        res.status(200).send({ commentData, commentCount });
+
+    } catch (err) {
+        console.log('error occur in comment', err.message)
+    }
+};
 
 module.exports = {
     createBlog,
-    showBlog,showBlogbyid,
+    showBlog, showBlogbyid,
     showBloguser,
     userdeleteBlog,
     userUpdateBlog,
