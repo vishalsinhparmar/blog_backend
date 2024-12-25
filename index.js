1c8f4f5d259234a424d8ee2e4e09f0f880feff9a
vishalsinh <vishalsinhparmar821@gamil.com>
2024-12-25 21:27:04 +0530

    cors

--- index.js
+++ index.js
@@ -10,8 +10,23 @@ app.use('/uploads',express.static('uploads'));
 mongoose.connect(`${process.env.MONGODB_URL}`)
 .then(()=> console.log('the dataBase is connected'))
 .catch((err)=>console.log(err.message))
+
+const whitelist = [
+  process.env.FRONTEND_URL, // Production frontend URL
+  'http://localhost:5174',  // Local development frontend
+]
+
+
+
 const corsOptions = {
-    origin:[process.env.FRONTEND_URL], // Allowed origins
+  origin: function (origin, callback) {
+    // Allow requests with no origin (like mobile apps or Postman)
+    if (!origin || whitelist.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }, // Allowed origins
     methods: ["GET", "POST", "PUT", "DELETE","PATCH"], // Allowed methods
     credentials: true, // Allow cookies if needed
     sameSite:'None'
